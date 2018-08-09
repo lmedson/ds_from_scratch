58b9820aac224785248376ffc0cd8687d2f4535d
lmedson <medson-mbp@hotmail.com>
2018-08-08 21:42:49 -0300

    Add new methods #2: friends of friends

--- cap1/friends_methods.py
+++ cap1/friends_methods.py
@@ -1,5 +1,23 @@
+from collections import Counter 
+
 def friends_of_friend_ids_bad(user):
     #"foaf" acrônimo friend of a friend
     return [foaf["id"]
             for friend in user["friends"] #para cada amigo de usuário
             for foaf in friend["friends"]] #para acada _their_friends
+
+def not_the_same(user, other_user):
+    # dois usuários não são os mesmos se possuírem ids diferentes
+    return user["id"] != other_user["id"]
+
+def not_friends(user, other_user):
+    return all(not_the_same(friend,other_user) for friend in user["friends"])
+
+def friends_of_friend_ids(user):
+    return Counter(foaf["id"]
+                    for friend in user["friends"] # para cada um de meus amigos
+                    for foaf in friend["friends"] # que contam *their* amigos
+                    if not_the_same(user, foaf)   # que não sejam eu
+                    and not_friends(user, foaf))  # e que não são meus amigos
+
+ 
